a9143e8b99dcd1f3ef72ba5e8034233e0a3a64ee
PL Deepak <pulukantilaxmideepak.com>
2023-03-25 14:15:40 +0530

    third push

--- freelancer.py
+++ freelancer.py
@@ -1,10 +1,13 @@
-'''def func(n):
+def func(n):
     print(n*8)
 func(int(input()))
-'''
+
 def func(n,r):
     print((n*176)-((n*176)*r))
-n=int(input())
-r=int(input())
-func(n,r)
-    
+func(int(input()),int(input()))
+
+def third(a,b,c):
+    d=(b*8)-(b*8*c)
+    e=a//d
+    print(e)
+third(int(input()),int(input()),float(input()))
