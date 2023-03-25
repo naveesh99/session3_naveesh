4c76cabb0c7556a88ac6968611de26210d19e1ca
unknown <ateeqrahman354@gmail.com>
2023-03-25 13:18:01 +0530

    Second Push

--- Freelancer.py
+++ Freelancer.py
@@ -1,4 +1,20 @@
-def func1(n):
+'''def func1(n):
     print(n*8)
 n=int(input())
-func1(n)
+func1(n)'''
+
+'''def func2(m,rate):
+    print((m*176)-((m*176)*rate))
+m=int(input())
+rate=float(input())
+func2(m,rate)'''
+
+
+
+
+
+
+
+
+
+
